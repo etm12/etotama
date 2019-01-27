12b71c5d287005d23229e16d337f1731f58af385
Stefan Rimaila <stefan@rimaila.fi>
2019-01-27 16:50:07 +0200

    feat(core/shared): Add initial functionality, type defs

---
+++ core/shared/lib/index.d.ts
@@ -0,0 +1,8 @@
+export function camelTokens(camelString: string): string[];
+export function kebabTokens(kebabString: string): string[];
+
+export function capitalize(word: string): string;
+export function camelKebab(kebabString: string): string;
+export function kebabCamel(camelString: string): string;
+
+export const obsObjectL: any;
---
+++ core/shared/lib/index.js
@@ -0,0 +1,39 @@
+// @ts-check
+/// <reference path="./index.d.ts" />
+import * as R from 'ramda';
+import * as L from 'partial.lenses';
+import * as K from 'kefir';
+
+// #region String parsing
+export const camelTokens = R.match(/(^[a-z]+|[A-Z][a-z]+)/g);
+export const kebabTokens = R.split('-');
+// #endregion
+
+// #region String manipulation
+export const capitalize = R.converge(
+  R.concat,
+  [
+    R.compose(R.toUpper, R.head),
+    R.compose(R.toLower, R.tail),
+  ],
+);
+
+export const camelKebab = L.transform(L.seq(
+  [L.modifyOp(R.split('-'))],
+  [L.slice(1, Infinity), L.elems, L.modifyOp(capitalize)],
+  [L.modifyOp(R.join(''))],
+));
+
+export const kebabCamel = L.transform(L.seq(
+  [L.modifyOp(camelTokens)],
+  [L.elems, L.modifyOp(R.toLower)],
+  [L.modifyOp(R.join('-'))],
+));
+// #endregion
+
+// #region OBS
+export const obsObjectL = L.lens(
+  L.modify(L.keys, camelKebab),
+  L.modify(L.keys, kebabCamel),
+);
+// #endregion
