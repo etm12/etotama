5a471098dad40114c18a417f81481e94db0c8871
Stefan Rimaila <stefan@rimaila.fi>
2019-01-31 15:11:46 +0200

    feat(apps/pixel:ui): Add Measure component

--- apps/pixel/src/components/ui/index.js
+++ apps/pixel/src/components/ui/index.js
@@ -2,3 +2,4 @@ export { default as Menubar } from './menubar';
 export { default as Menuitem } from './menuitem';
 export { default as Panel } from './panel';
 export { default as SidebarPanel } from './sidebar-panel';
+export { default as Measure } from './measure';
