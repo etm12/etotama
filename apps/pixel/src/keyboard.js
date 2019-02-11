d4e4780b39c0c14b04bb2feb5aa831fdb7d01fda
Stefan Rimaila <stefan@rimaila.fi>
2019-02-11 11:13:08 +0200

    feat(app.pixel): Add global keyboard event listeners

---
+++ apps/pixel/src/keyboard.js
@@ -0,0 +1,12 @@
+import * as U from 'karet.util';
+import * as R from 'kefir.ramda';
+// eslint-disable-next-line
+import * as K from 'kefir';
+
+const takeEvent = type => U.thru(
+  U.fromEvents(document, type, R.identity),
+  U.toProperty,
+);
+
+/** @type {K.Property<KeyboardEvent, any>} */
+export const onKeyDown = takeEvent('keydown');
