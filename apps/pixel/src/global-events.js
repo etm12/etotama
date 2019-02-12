c5d379db0e7fea8580e3590c30c11cec3d868bf8
Stefan Rimaila <stefan@rimaila.fi>
2019-02-12 11:35:30 +0200

    feat(app.pixel): Add global undo/redo event

--- apps/pixel/src/global-events.js
+++ apps/pixel/src/global-events.js
@@ -4,6 +4,8 @@ import * as R from 'kefir.ramda';
 import * as KeyboardEvent from './keyboard';
 
 const whereCode = code => R.whereEq({ code });
+const withMetaKeyAnd = p => R.both(R.whereEq({ metaKey: true }), p);
 const takeOnly = U.skipUnless;
 
 export const onSwitchCurrentColors = takeOnly(whereCode('KeyX'), KeyboardEvent.onKeyDown);
+export const onDoUndo = takeOnly(withMetaKeyAnd(whereCode('KeyZ')), KeyboardEvent.onKeyDown);
