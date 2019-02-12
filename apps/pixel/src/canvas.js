1a6ed28000432889e63c652e7b202ffad393c8fa
Stefan Rimaila <stefan@rimaila.fi>
2019-02-12 11:36:07 +0200

    feat(app.pixel): Add image save functionality

--- apps/pixel/src/canvas.js
+++ apps/pixel/src/canvas.js
@@ -3,6 +3,23 @@ import * as K from 'kefir';
 import * as U from 'karet.util';
 import * as S from '@etotama/core.shared';
 import { Canvas } from './containers/editor/meta';
+import { saveAs } from 'file-saver';
+
+//
+
+export const saveImageData = ({ name, imageData, width, height }) => e => {
+  const canvas = document.createElement('canvas');
+  Object.assign(canvas, { width, height });
+
+  const data = new ImageData(imageData, width);
+
+  const ctx = canvas.getContext('2d');
+  ctx.putImageData(data, 0, 0);
+
+  canvas.toBlob(b => saveAs(b, `${name}.png`));
+};
+
+export const saveImageDataU = U.liftRec(saveImageData);
 
 const viewCoordIndex = ({ sampleBy, pixel, width }) => U.thru(
   U.combine(
