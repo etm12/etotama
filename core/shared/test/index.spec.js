188cf191d6b5a296dd8b20c647cc161fdbb79b02
Stefan Rimaila <stefan@rimaila.fi>
2019-02-18 19:03:43 +0200

    test(core.shared): Improve tests

--- core/shared/test/index.spec.js
+++ core/shared/test/index.spec.js
@@ -1,100 +1,72 @@
 import * as C from 'd3-color';
+import * as R from 'kefir.ramda';
 import * as S from '../lib/index';
+import { testEq } from './util';
 
-//
+const runTests = R.pipe(R.filter(x => x.length), R.forEach(R.apply(testEq)));
 
-test(S.flip.name, () => {
-  const fn = (a, b) => a - b;
-  expect(S.flip(fn)(5, 0)).toBe(fn(0, 5));
+describe('Functions', () => {
+  runTests([
+    [-5, () => S.flip((a, b) => a - b)(5, 0)],
+    [[1, 2, 3], () => S.apply((a, b, c) => [a, b, c], [1, 2, 3])],
+    ['[object Object]', () => S.invoke0('toString', {})],
+    ['this-that', () => S.invoke1('join', '-', ['this', 'that'])],
+    [[1, 2], () => S.invoke2('foo', 1, 2, { foo: (a, b) => [a, b] })],
+    ['called', () => S.call0(() => 'called')],
+  ]);
 });
 
-test(S.apply.name, () => {
-  const fn = (a, b, c) => [a, b, c];
-  const input = [1, 2, 3];
-  const expected = [1, 2, 3];
-  expect(S.apply(fn, input)).toEqual(expected);
-  expect(S.apply(fn)(input)).toEqual(expected);
+describe('Strings', () => {
+  runTests([
+    [['this', 'That', 'Then'], () => S.camelTokens('thisThatThen')],
+    [['this', 'that', 'then'], () => S.kebabTokens('this-that-then')],
+    ['Foo', () => S.capitalize('foo')],
+  ]);
+
+  test.todo(S.camelKebab.name);
+  test.todo(S.kebabCamel.name);
 });
 
-test(S.invoke0.name, () => {
-  const fn = S.invoke0('toString');
-  const input = {};
-  const expected = '[object Object]';
-  expect(fn(input)).toBe(expected);
+describe('Arrays', () => {
+  runTests([
+    [4, () => S.computeIx(1, 0, 10)],
+    [8, () => S.computeIx(2, 0, 10)],
+    [{ start: 4, end: 8 }, () => S.getIx(1, 0, 10)],
+  ]);
 });
 
-test(S.invoke1.name, () => {
-  const fn = S.invoke1('join', '-');
-  const input = ['this', 'that'];
-  const expected = 'this-that';
-  expect(fn(input)).toBe(expected);
+describe('Colors', () => {
+  test(S.toColor.name, () => {
+    const x = S.toColor('#f00');
+    expect(x).toBeInstanceOf(C.color);
+  });
+
+  test(S.toHex.name, () => {
+    const c = S.toColor('#f00');
+    const x = S.toHex(c);
+    expect(x).toBe('#ff0000');
+  });
+
+  test(S.darken.name, () => {
+    const c = S.toColor('#f00');
+    const x = S.darken(1, c);
+    const x2 = S.darken(1)(c);
+    expect(x.hex()).toBe('#b30000');
+    expect(x2.hex()).toBe('#b30000');
+  });
 });
 
-test(S.invoke2.name, () => {
-  const fn = S.invoke2('foo', 1, 2);
-  const input = { foo: (a, b) => [a, b] };
-  const output = [1, 2];
-  expect(fn(input)).toEqual(output);
-})
-
-test(S.call0.name, () => {
-  const fn = jest.fn();
-  S.call0(fn);
-
-  expect(fn).toBeCalled();
-})
-
-test(S.camelTokens.name, () => {
-  expect(S.camelTokens('thisThatThen')).toEqual(['this', 'That', 'Then']);
+describe('Events', () => {
+  test.todo(S.persist.name);
+  test.todo(S.eventBus.name);
+  test.todo(S.takeEvent.name);
+  test.todo(S.takeEventU.name);
 });
 
-test(S.kebabTokens.name, () => {
-  expect(S.kebabTokens('this-that-then')).toEqual(['this', 'that', 'then'])
-});
+describe('Canvas', () => {
+  test.todo(S.getBoundingRect.name);
+  test.todo(S.getContext.name);
 
-test(S.capitalize.name, () => {
-  expect(S.capitalize('foo')).toBe('Foo');
-});
-
-test.todo(S.camelKebab.name);
-test.todo(S.kebabCamel.name);
-test.todo(S.persist.name);
-
-test.todo(S.getBoundingRect.name);
-test.todo(S.getContext.name);
-
-test(S.computeIx.name, () => {
-  const { x, y, w } = { x: 1, y: 0, w: 10 };
-  expect(S.computeIx(x, y, w)).toBe(4);
-});
-
-test(S.getIx.name, () => {
-  const { x, y, w } = { x: 1, y: 0, w: 10 };
-  expect(S.getIx(x, y, w)).toEqual({ start: 4, end: 8 });
-});
-
-test.todo(S.eventBus.name);
-test.todo(S.takeEvent.name);
-test.todo(S.takeEventU.name);
-
-test.todo(S.offsetPositionBy.name);
-test.todo(S.scalePositionBy.name);
-
-test(S.toColor.name, () => {
-  const x = S.toColor('#f00');
-  expect(x).toBeInstanceOf(C.color);
-});
-
-test(S.toHex.name, () => {
-  const c = S.toColor('#f00');
-  const x = S.toHex(c);
-  expect(x).toBe('#ff0000');
-});
-
-test(S.darken.name, () => {
-  const c = S.toColor('#f00');
-  const x = S.darken(1, c);
-  const x2 = S.darken(1)(c);
-  expect(x.hex()).toBe('#b30000');
-  expect(x2.hex()).toBe('#b30000');
+  test.todo(S.offsetPositionBy.name);
+  test.todo(S.scalePositionBy.name);
 });
---
+++ core/shared/test/util.js
@@ -0,0 +1,47 @@
+/**
+ * Test utilities borrowed from [karet.util's](https://github.com/calmm-js/karet.util/blob/master/test/tests.js)
+ * tests.
+ */
+import { Observable } from 'kefir';
+import * as U from 'karet.util';
+import * as R from 'kefir.ramda';
+
+export function show(x) {
+  switch (typeof x) {
+    case 'string':
+    case 'object':
+      return JSON.stringify(x);
+    default:
+      return `${x}`;
+  }
+}
+
+export const toExpr = f =>
+  f
+    .toString()
+    .replace(/\s+/g, ' ')
+    .replace(/^\s*function\s*\(\s*\)\s*{\s*(return\s*)?/, '')
+    .replace(/\s*;?\s*}\s*$/, '')
+    .replace(/function\s*(\([a-zA-Z]*\))\s*/g, '$1 => ')
+    .replace(/{\s*return\s*([^{;]+)\s*;\s*}/g, '$1')
+    .replace(/\(([a-zA-Z0-9_]+)\) =>/g, '$1 =>')
+    .replace(/\(0, _kefir[^.]*[.]constant\)/g, 'C')
+    .replace(/_kefir[^.][.]/g, '');
+
+export const testEq = (expect, thunk) =>
+  it(`${toExpr(thunk)} => ${show(expect)}`, done => {
+    const actual = thunk();
+    const check = actual => {
+      const eq = R.equals(actual, expect);
+      if (eq instanceof Observable || !eq) {
+        done(Error(`Expected: ${show(expect)}, actual: ${show(actual)}`));
+      } else {
+        done();
+      }
+    }
+    if (actual instanceof Observable) {
+      U.thru(actual, U.takeFirst(1), U.on({value: check, error: check}));
+    } else {
+      check(actual);
+    }
+  });
